58d97e2b11d78a553333207daed22bfa0e8f5784
SangamMundhe <sangammunde3@gmail.com>
2023-12-16 17:41:23 +0530

    Add files via upload

---
+++ Day6/BitManipulation/ClearIthbit.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ClearIthbit{
+
+    public static int clearIthBit(int n , int i){
+
+        int bitmask = ~(1<<i);
+
+        return n & bitmask;
+    }
+
+    public static void main(String args[]){
+                System.out.println(clearIthBit(5,2));
+
+
+
+    }
+}
