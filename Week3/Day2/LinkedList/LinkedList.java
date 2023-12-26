8466fb28394fb96e6eca3cbc4abccce62cade0f2
SangamMundhe <sangammunde3@gmail.com>
2023-12-26 17:51:02 +0530

    Update LinkedList.java
    1.Add  at first position in Linkedlist

--- Week3/Day2/LinkedList/LinkedList.java
+++ Week3/Day2/LinkedList/LinkedList.java
@@ -10,10 +10,28 @@ public class LinkedList{
 
   public static Node head;
   public static Node tail;
+
+  // Add first Operation
+  // Add Element at first position of linked list
+  public static void addFirst(int data){
+    // Create the Newnode
+    Node newnode =new Node(data);
+
+    if(head == null){
+      head=tail=newnode;
+      return;
+    }
+    // assign nodes next equals to head
+    newnode.next =head;
+    head = newnode;
     
+  }
+
   public static void main(String args[]){
     LinkedList l1  = new LinkedList();
-    l1.head = new Node(1);
-    l1.head.next = new Node(2);
+    // l1.head = new Node(1);
+    // l1.head.next = new Node(2);
+    l1.addFirst(1);
+    l1.addFirst(2);
   }
 }
