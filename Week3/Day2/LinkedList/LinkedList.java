59b685e70167e4ac40c82946d407170cfa12ab1e
SangamMundhe <sangammunde3@gmail.com>
2023-12-26 18:26:08 +0530

    Update LinkedList.java

--- Week3/Day2/LinkedList/LinkedList.java
+++ Week3/Day2/LinkedList/LinkedList.java
@@ -24,8 +24,8 @@ public class LinkedList{
     
   }
 
-  // Add at Last Position 
-   public void addLast(int data){
+  // ADD Last Operation 
+  public void addLast(int data){
     Node newnode  = new Node(data);// 1.Create the newNode
     if(head == null){           // case - head and tail are equal to null means linked list is empty
       head=tail= newnode;
@@ -33,16 +33,40 @@ public class LinkedList{
     }
     tail.next = newnode;    // 2.tails next will newnode
     tail =newnode;          // 3.tail = newnode
+    
+  }
+
+  // Print The linked list
+  public static void printList(){
+    if(head == null){
+      System.out.println("Linkedlist is Empty");
+      return;
+    }
+    Node temp = head;
+    while(temp != null){
+      System.out.print(temp.data+"->");
+      temp =temp.next;
+    }
+    System.out.println("null");
+
+
   }
 
   public static void main(String args[]){
     LinkedList l1  = new LinkedList();
     // l1.head = new Node(1);
     // l1.head.next = new Node(2);
-    l1.addFirst(1);
+    printList();
     l1.addFirst(2);
+    printList();
+
+    l1.addFirst(1);
+    printList();
 
     l1.addLast(3);
+    printList();
+    
     l1.addLast(4);
+    printList();
   }
 }
