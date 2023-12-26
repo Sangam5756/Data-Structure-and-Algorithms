a9a3dcec429ad0850040975f0b27b4177013710e
SangamMundhe <sangammunde3@gmail.com>
2023-12-26 18:17:18 +0530

    Update LinkedList.java

--- Week3/Day2/LinkedList/LinkedList.java
+++ Week3/Day2/LinkedList/LinkedList.java
@@ -14,17 +14,25 @@ public class LinkedList{
   // Add first Operation
   // Add Element at first position of linked list
   public static void addFirst(int data){
-    // Create the Newnode
-    Node newnode =new Node(data);
-
+    Node newnode =new Node(data);   // Create the Newnode
     if(head == null){
-      head=tail=newnode;
+          head=tail=newnode;
+          return;
+      }
+      newnode.next =head;        // newnodes next equals to head
+      head = newnode;           // now head is newnode
+    
+  }
+
+  // Add at Last Position 
+   public void addLast(int data){
+    Node newnode  = new Node(data);// 1.Create the newNode
+    if(head == null){           // case - head and tail are equal to null means linked list is empty
+      head=tail= newnode;
       return;
     }
-    // assign nodes next equals to head
-    newnode.next =head;
-    head = newnode;
-    
+    tail.next = newnode;    // 2.tails next will newnode
+    tail =newnode;          // 3.tail = newnode
   }
 
   public static void main(String args[]){
@@ -33,5 +41,8 @@ public class LinkedList{
     // l1.head.next = new Node(2);
     l1.addFirst(1);
     l1.addFirst(2);
+
+    l1.addLast(3);
+    l1.addLast(4);
   }
 }
