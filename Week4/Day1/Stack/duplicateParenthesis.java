59fe661630a56609a97808520e87b2e43b79a3f7
SangamMundhe <sangammunde3@gmail.com>
2024-01-01 22:15:22 +0530

    Create duplicateParenthesis.java

---
+++ Week4/Day1/Stack/duplicateParenthesis.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class DuplicateParenthesis{
+
+    public static boolean duplicateParenthesis(String str){
+
+        Stack <Character>  s = new Stack<>();
+
+        for(int i =0; i<str.length(); i++){
+
+            char ch =str.charAt(i);
+            if(ch == ')'){
+                int count =0;
+                while(s.peek() !='('){
+                    s.pop();
+                    count++;
+                }
+                if(count <1){
+                    return true;
+                }
+                else{
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+
+        String str = "(((a+b)+(a+b)))";
+        String str1 = "((a+b)+(a+b))";
+        System.out.println(duplicateParenthesis(str)); //true
+        System.out.println(duplicateParenthesis(str1));  //false
+
+
+    }
+}
