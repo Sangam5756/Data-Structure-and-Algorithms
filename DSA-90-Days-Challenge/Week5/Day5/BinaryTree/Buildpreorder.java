3fb747ce82ea053a6c0ab81331e281a9f893cc31
Sangam5756 <sangammunde3@gmail.com>
2024-01-13 13:18:28 +0530

    updation

--- DSA-90-Days-Challenge/Week5/Day5/BinaryTree/Buildpreorder.java
+++ DSA-90-Days-Challenge/Week5/Day5/BinaryTree/Buildpreorder.java
@@ -31,10 +31,20 @@ public class Buildpreorder{
 
             return newNode;
 
+        }
+        public static void preorder(Node root){
+
+            if( root == null){
+                return;
+            }
+            System.out.print(root.data+" ");
+            preorder(root.left);
+            preorder(root.right);
+
 
         }
     }
-
+    
     public static void main(String args[]){
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
@@ -42,7 +52,8 @@ public class Buildpreorder{
 
         Node root = tree.buildTree(nodes);
 
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        tree.preorder(root);
 
 
     }
