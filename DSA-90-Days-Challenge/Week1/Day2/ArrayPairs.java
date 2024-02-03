0510e24d82486036ef69110128e5eccc14ec7584
SangamMundhe <sangammunde3@gmail.com>
2024-02-03 20:29:07 +0530

    Update ArrayPairs.java

--- DSA-90-Days-Challenge/Week1/Day2/ArrayPairs.java
+++ DSA-90-Days-Challenge/Week1/Day2/ArrayPairs.java
@@ -3,23 +3,20 @@ import java.util.*;
 public class ArrayPairs{
 
   //function for the Pairs
-   public static void Pairs(int arr[]){
-            int totalpair =0;   //intitially total pair is 0
-        for(int i=0; i<arr.length; i++){
-            for(int j =i+1; j<arr.length;j++){
-                System.out.print("(" +arr[i]+ "," +arr[j]+ ")"+" "); //print the pair
-                totalpair++; // increment total pair counter
-            }
-            System.out.println();
-        }
-        System.out.println("Total pair is :"+totalpair); // print the total pair
+public static void pairs(int arr[]){
+  for(int i=0; i<arr.length; i++){
+    for(int j = i+1; j<arr.length; j++){
+      System.out.print("("+ arr[i]+ ","+ arr[j] + ")" );
     }
+    System.out.println();
+  }
+}
 
   //driver code
   public static void main(String args[]){
     int arr[] ={2,4,6,8,10};
    
-     Pairs(arr); //function call
+     pairs(arr); //function call
     
   }
 }
