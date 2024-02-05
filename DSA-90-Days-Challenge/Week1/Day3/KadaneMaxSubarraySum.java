cc898cea6764266e0bdbb6d93ceccf7e6aa856c3
Sangam5756 <sangammunde3@gmail.com>
2024-02-05 20:24:47 +0530

    updated

--- DSA-90-Days-Challenge/Week1/Day3/KadaneMaxSubarraySum.java
+++ DSA-90-Days-Challenge/Week1/Day3/KadaneMaxSubarraySum.java
@@ -18,7 +18,8 @@ public class KadaneMaxSubarraySum{
         System.out.println("Array Elements are Negative:"+check);}
         
         
-        if(check != -1){for(int i =0;  i<arr.length;i++){
+        if(check != -1){
+            for(int i =0;  i<arr.length;i++){
             sum = sum+arr[i];
             if(sum <0){
                 sum =0;
@@ -29,11 +30,33 @@ public class KadaneMaxSubarraySum{
         System.out.println("Max Subarray Sum is :"+maxsum);}
     }
 
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");}
+    }
+
+
+
+    public static int maxSum(int nums[]){
+        int max = Integer.MIN_VALUE;
+        int curr=0;
+        for(int i=0; i<nums.length; i++){                
+                curr +=nums[i];
+                if( curr < 0){
+                curr= 0;
+            }
+            max =Math.max(curr, max);
+        }
+        return max;
+    }
+
     public static void main(String args[]){
         int arr[] ={-2,-3,4,-1,-2,1,5,-3};
         // int arr[] ={-2,-3};
 
         maxSubarraySum(arr);
+        System.out.println(maxSum(arr));
+        
 
 
     }
