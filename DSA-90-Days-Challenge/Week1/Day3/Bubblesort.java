4b61b86fa5162d69d3eee03e29a4a5ce81dd2552
Sangam Mundhe <sangammunde3@gmail.com>
2024-02-11 17:47:35 +0530

    Update Bubblesort.java

--- DSA-90-Days-Challenge/Week1/Day3/Bubblesort.java
+++ DSA-90-Days-Challenge/Week1/Day3/Bubblesort.java
@@ -1,33 +1,31 @@
-import java.util.*;
-
-public class Bubblesort{
-    public static void print(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-    }
-
-    public static void bubbleSort(int arr[]){
+public class Bubblesort {
+    
+    public static void bubble(int arr[]){
 
         for(int i=0; i<arr.length; i++){
-            for(int j =i+1;j<arr.length;j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[j];
-                    arr[j] =arr[i];
-                    arr[i] =temp;
 
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] > arr[j]){
+                    int temp= arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
+                
             }
+        }
+    }
+
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i] +" ");
         }
     }
 
     public static void main(String args[]){
 
-        int arr[] ={5,4,3,2,1};
-        print(arr);
-        bubbleSort(arr);
-        print(arr);
+        int arr[] = {5,4,8,2,1};
+        bubble(arr);
+        printarr(arr);
 
     }
 }
