22b5aaa93d91e8d520184189893bbf757d78bdac
Sangam Mundhe <sangammunde3@gmail.com>
2024-07-07 11:51:10 +0530

    Create floor-and-ceil-in-sorted-array.java

---
+++ LEETCODE/1-D-Array/floor-and-ceil-in-sorted-array.java
@@ -0,0 +1,45 @@
+// Problem Statement: You're given an sorted array arr of n integers and an integer x. Find the floor and ceiling of x in arr[0..n-1].
+// The floor of x is the largest element in the array which is smaller than or equal to x.
+// The ceiling of x is the smallest element in the array greater than or equal to x.
+
+
+static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                low = mid + 1;
+            } else {
+                high = mid - 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int f = findFloor(arr, n, x);
+        int c = findCeil(arr, n, x);
+        return new int[] {f, c};
+    }
